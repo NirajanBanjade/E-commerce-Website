e7ccb2bbc989cd6764514f52f4c35cb7a135448f
Nirajan Banjade <nirajanbanjade321@gmail.com>
2025-01-23 22:42:51 -0600

    frontend on vercel test

--- front/src/constants.js
+++ front/src/constants.js
@@ -8,7 +8,7 @@
 
 export const BASE_URL = process.env.NODE_ENV === 'development' 
   ? 'http://localhost:5001' 
-  : '';  // Add production URL if applicable
+  : 'https://e-commerce-website-7-665c.onrender.com';  // Add production URL if applicable
 
 export const PRODUCTS_URL = `${BASE_URL}/api/List`;
 export const USERS_URL = `${BASE_URL}/api/users`;
