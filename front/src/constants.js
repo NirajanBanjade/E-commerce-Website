4bd4a55e2446086546b86b9352b2e6965e6e9cd4
Nirajan Banjade <nirajanbanjade321@gmail.com>
2025-01-25 12:24:17 -0600

    vercel setup

--- front/src/constants.js
+++ front/src/constants.js
@@ -6,9 +6,7 @@
 // export const ORDERS_URL = '/api/orders';
 // export const PAYPAL_URL = '/api/config/paypal';
 
-export const BASE_URL = process.env.NODE_ENV === 'development' 
-  ? 'http://localhost:5001' 
-  : 'https://e-commerce-website-11-8obv.onrender.com/';  // Add production URL if applicable
+export const BASE_URL = 'https://e-commerce-website-11-8obv.onrender.com/';  // Add production URL if applicable
 
 export const PRODUCTS_URL = `${BASE_URL}/api/List`;
 export const USERS_URL = `${BASE_URL}/api/users`;
